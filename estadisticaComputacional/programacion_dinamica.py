67ba865509f80ae3c9e14616b885a59f9c7e4620
SilviaAlarcon <silvia.alarcon.or@gmail.com>
2020-11-02 02:01:28 -0600

    Curso de estadistica computacional con python - programacion dinamica

---
+++ estadisticaComputacional/programacion_dinamica.py
@@ -0,0 +1,24 @@
+import sys
+
+def fibonacci_recursivo(n):
+    if n == 0 or n == 1:
+        return 1 
+
+    return fibonacci_recursivo(n - 1) + fibonacci_recursivo(n - 2)
+
+def fibonacci_dinamico(n, memo = {}):
+    if n == 0 or n == 1:
+        return 1
+
+    try:
+        return memo[n]
+    except KeyError:
+        result = fibonacci_dinamico(n - 1, memo) + fibonacci_dinamico(n - 2, memo)
+        memo[n] = result
+        return result
+
+if __name__ == "__main__":
+    sys.setrecursionlimit(10003) #para cambiar el limite de recursividad 
+    n = int(input('Escoge un numero: '))
+    result = fibonacci_dinamico(n)
+    print(result)
